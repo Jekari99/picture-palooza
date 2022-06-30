0ae69055b3870d9ae060488715dfd8f69cfddc84
Jekari Rawls <jekarirawls@gmail.com>
2022-06-29 20:06:55 -0400

    Fixed Scroll Button

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,24 +1,23 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 function ScrollButton() {
-    // const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false)
 
-    // const toggleVisible = () => {
-    //     const scrolled = document.documentElement.scrollTop;
-    //     if (scrolled > 300) {
-    //         setVisible(true)
-    //     }
-    //     else if (scrolled <= 300) {
-    //         setVisible(false)
-    //     }
-    // };
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true)
+        }
+        else if (scrolled <= 300) {
+            setVisible(false)
+        }
+    };
 
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
-            /* you can also use 'auto' behaviour
-               in place of 'smooth' */
+
         });
     };
 
@@ -26,7 +25,7 @@ function ScrollButton() {
 
     return (
         <div className="topButton">
-            <button className="backToTop" onClick={scrollToTop}>Back To Top</button>
+            <button className="backToTop" onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }}>Back To Top</button>
         </div>
     );
 }
