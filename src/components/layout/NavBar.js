2196e50e521e2c3dda66bd758eb3d3193bf0a7a5
Jekari Rawls <jekarirawls@gmail.com>
2022-07-30 14:04:58 -0400

    Fixed responsive navbar + styling

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,23 +1,30 @@
-import classes from "./NavBar.module.css";
+import "./NavBar.css";
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 function NavBar() {
 
+    const [nav, setNav] = useState(false);
+
+    const showNav = () => {
+        setNav(!nav);
+    }
+
 
     return (
-        <header className={classes.header}>
-            <div className={classes.title}>
+        <header className='header'>
+            <div className='title'>
                 <Link to="/">Picture Palooza</Link>
-                <span className={classes.hamIcon}>
-                    <button><i className="fa fa-bars"></i></button>
+                <span className='hamIcon'>
+                    <button onClick={showNav}><i className="fa fa-bars"></i></button>
                 </span>
             </div>
 
-            <nav className={classes.nav}>
-                <ul className={classes.list}>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/search'>Search</Link></li>
-                    <li><Link to='/about'>About</Link></li>
+            <nav className={nav ? "expand" : "nav"}>
+                <ul className='list'>
+                    <li onClick={showNav}><Link to='/'>Home</Link></li>
+                    <li onClick={showNav}><Link to='/search'>Search</Link></li>
+                    <li onClick={showNav}><Link to='/about'>About</Link></li>
 
 
                 </ul>
